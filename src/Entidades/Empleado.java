5834d8ec4779dadc24f1147a2970903bb2c3a1cc
Federico Hernandez <fede_h129@hotmail.com>
2021-10-18 16:35:06 -0300

    Primer Commit nuevarama

---
+++ src/Entidades/Empleado.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author fede_
+ */
+public class Empleado {
+    private Integer id;
+    private String apellido;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+    
+    
+}
