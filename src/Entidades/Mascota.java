51f354d170d49520696dcefb6455df5d3427ce94
Federico Hernandez <fede_h129@hotmail.com>
2021-10-18 16:23:18 -0300

    Segundo Commit

---
+++ src/Entidades/Mascota.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entidades;
+
+/**
+ *
+ * @author fede_
+ */
+public class Mascota {
+    private Integer id;
+    private String nombre;
+
+    public Mascota() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    
+}
